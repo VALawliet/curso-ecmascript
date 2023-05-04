785d6492f01b9acbf65d2a16e09142dc4dabc58d
Law <victorestrada2006@gmail.com>
2023-05-04 03:40:01 +0200

    Agregando información sobre los sets y sus métodos .add().

---
+++ src/es6/10-set-add.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+//Generando un Set()
+const list = new Set();
+
+//Utilizando el método .add() par aañadir elementos al nuevo set
+
+list.add(1);
+
+//Se pueden poner dos .add() juntos en la misma línea.
+list.add(2).add(3);
+
+console.log(list);
